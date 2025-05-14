01959b8b46e7f4ca010b90ccd87fa8b93b59e900
Malek04 <malek.aiter03@gmail.com>
2025-05-14 16:10:31 +0100

    add docs

---
+++ docs/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { ChatComponent } from './Scénario/chat/chat.component';
+
+const routes: Routes = [
+  {path:'', component: MainComponent},
+  {path:'scenario1', component: ChatComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ docs/src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { HeroComponent } from './shared/hero/hero.component';
+import { CardsComponent } from './shared/cards/cards.component';
+import { ChatComponent } from './Scénario/chat/chat.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { QuestionDialogComponent } from './Scénario/questions/questions.component';
+import { CommonModule } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { RetryModalComponent } from './Scénario/retry-modal/retry-modal.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    MainComponent,
+    HeroComponent,
+    CardsComponent,
+    ChatComponent,
+    QuestionDialogComponent,
+    RetryModalComponent,
+  ],
+  imports: [
+    MatDialogModule,
+    CommonModule,
+    RouterModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    HttpClientModule,
+    FormsModule,
+    MatIconModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
